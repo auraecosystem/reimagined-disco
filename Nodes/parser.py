ccceab7a999964164e309d977e9dc7431ac9f4c3
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-23 00:18:33 +0100

    Create parser.py

---
+++ Nodes/parser.py
@@ -0,0 +1,11 @@
+class Program:
+    def __init__(self):
+        self.statements = []
+
+class Agent:
+    def __init__(self, name):
+        self.name = name
+
+class Vector:
+    def __init__(self, values):
+        self.values = values
