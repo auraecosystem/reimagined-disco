4ee7a9a53e9d3499268da442fd21e1088472d233
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-23 01:19:51 +0100

    Update server listening address and port

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ const server = createServer((req, res) => {
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:8000');
+  console.log('Listening on 192.168.1.1:3000');
 });
 
 // run with `node server.mjs`
